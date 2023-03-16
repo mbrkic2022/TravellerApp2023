308ce91104e58f1fb44041743518cd61d52fe1f9
korisnik <korisnik@ODJ-O366-101>
2023-03-16 11:16:34 +0100

    Add project files.

---
+++ TravellerApp/TravellerApp/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravellerApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewTravelPage());
+        }
+    }
+}
---
+++ TravellerApp/TravellerApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravellerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrWhiteSpace(EmailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrWhiteSpace(PasswordEntry.Text);
+            if (!isEmailEmpty && !isPasswordEmpty)
+            {
+                await Navigation.PushAsync(new HomePage());
+            }
+            else 
+            {
+                await DisplayAlert("E-mail and password required", "Please enter both e-mail and password!", "OK");
+            }
+        }
+    }
+}
