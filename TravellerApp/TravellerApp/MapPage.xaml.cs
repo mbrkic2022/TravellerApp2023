6dc59197f53bec03944d0e9ae11e4dea3d31cddd
mbrkic2022 <mbrkic@uniri.hr>
2023-03-30 10:01:42 +0200

    Added asking for permission to use current location

--- TravellerApp/TravellerApp/MapPage.xaml.cs
+++ TravellerApp/TravellerApp/MapPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,29 @@ namespace TravellerApp
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+            var status = await CheckAndRequestLocationPermission();
+            if (status == PermissionStatus.Granted)
+            {
+                var location=await Geolocation.GetLocationAsync();
+            }
+        }
+
+        private async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status== PermissionStatus.Granted) { return status; }
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
         }
     }
 }
