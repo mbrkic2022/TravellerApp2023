ce4c0a59c028652b67642415066ffe1a117780d3
Marija Brkić Bakarić <mbrkic@uniri.hr>
2023-03-31 09:10:18 +0200

    Data model updated

--- TravellerApp/TravellerApp/Model/Post.cs
+++ TravellerApp/TravellerApp/Model/Post.cs
@@ -11,5 +11,10 @@ namespace TravellerApp.Model
         public int Id { get; set; }
         [MaxLength(250)]
         public string Experience { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string Municipality { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
