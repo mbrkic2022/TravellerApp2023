bd7644906ddaaae05e52e6c81393e64556aebc47
mbrkic2022 <mbrkic@uniri.hr>
2023-04-13 11:21:14 +0200

    Added RESTful service TomTom (Reverse Geocoding API)

---
+++ TravellerApp/TravellerApp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravellerApp.Helpers
+{
+    public class Constants
+    {
+        public const string LOCATION_SEARCH = "https://{0}/search/{1}/reverseGeocode/{2}.json?key={3}";
+        public const string BASE_URL = "api.tomtom.com";
+        public const string VERSION_NUM = "2";
+    }
+}
